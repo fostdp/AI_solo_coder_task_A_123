56f30f19d4a639d6d643c45a253ab8ef943736cf
fostdp <129711106+fostdp@users.noreply.github.com>
2026-06-15 13:21:29 +0800

    Update project

---
+++ backend/src/main/java/com/juyan/barracks/algorithm/NutritionFeature.java
@@ -0,0 +1,26 @@
+package com.juyan.barracks.algorithm;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class NutritionFeature {
+    private BigDecimal dailyProteinG;
+    private BigDecimal dailyFatG;
+    private BigDecimal dailyVitaminCMg;
+    private BigDecimal dailyCalorieKcal;
+    private BigDecimal calorieBurned;
+    private Integer age;
+    private Integer activityDays;
+    private BigDecimal avgProtein7d;
+    private BigDecimal avgFat7d;
+    private BigDecimal avgVitaminC7d;
+}
